fb60bd83e72e91ff631565c9537462058b8b22db
Sofya <vanieva.sofya@mail.ru>
2023-09-03 14:41:08 +0300

    Finaly054

--- Task054/Program.cs
+++ Task054/Program.cs
@@ -1,62 +1,84 @@
 ﻿using System;
+using System.Linq;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        int[,] array = CreateArray();
-        PrintArray(array);
-        SortRowsDescending(array);
-        Console.WriteLine("Сортировка:");
-        PrintArray(array);
-    }
+class Program 
+{ 
+    static void Main(string[] args) 
+    { 
+        int[,] array = CreateArray(); 
+        PrintArray(array); 
+        SortRowsDescending(array); 
+        Console.WriteLine("Сортировка:"); 
+        PrintArray(array); 
+    } 
+ 
+    static int[,] CreateArray() 
+    { 
+        int rows = 0;
+        int columns = 0;
+        bool isRowsValid = false;
+        bool isColumnsValid = false;
 
-    static int[,] CreateArray()
-    {
-        Console.Write("Введите необходимое количество строк: ");
-        int rows = int.Parse(Console.ReadLine());
-        Console.Write("Введите необходимое количество столбцов: ");
-        int columns = int.Parse(Console.ReadLine());
-        int[,] array = new int[rows, columns];
-        for (int i = 0; i < rows; i++)
+        while (!isRowsValid)
         {
-            for (int j = 0; j < columns; j++)
-            {
-                Console.Write($"Введите элемент [{i}, {j}]: ");
-                array[i, j] = int.Parse(Console.ReadLine());
-            }
+            Console.Write("Введите необходимое количество строк: "); 
+            isRowsValid = int.TryParse(Console.ReadLine(), out rows);
         }
-        return array;
-    }
 
-    static void PrintArray(int[,] array)
-    {
-        for (int i = 0; i < array.GetLength(0); i++)
+        while (!isColumnsValid)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                Console.Write(array[i, j] + " ");
-            }
-            Console.WriteLine();
+            Console.Write("Введите необходимое количество столбцов: "); 
+            isColumnsValid = int.TryParse(Console.ReadLine(), out columns);
         }
-    }
 
-    static void SortRowsDescending(int[,] array)
-    {
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1) - 1; j++)
-            {
-                for (int k = j + 1; k < array.GetLength(1); k++)
+        int[,] array = new int[rows, columns]; 
+
+        for (var i = 0; i < rows; i++) 
+        { 
+            for (var j = 0; j < columns; j++) 
+            { 
+                bool isElementValid = false;
+                int element = 0;
+
+                while (!isElementValid)
                 {
-                    if (array[i, j] < array[i, k])
-                    {
-                        int temp = array[i, j];
-                        array[i, j] = array[i, k];
-                        array[i, k] = temp;
-                    }
+                    Console.Write($"Введите элемент [{i}, {j}]: "); 
+                    isElementValid = int.TryParse(Console.ReadLine(), out element);
                 }
-            }
-        }
-    }
+
+                array[i, j] = element; 
+            } 
+        } 
+
+        return array; 
+    } 
+ 
+    static void PrintArray(int[,] array) 
+    { 
+        for (var i = 0; i < array.GetLength(0); i++) 
+        { 
+            for (var j = 0; j < array.GetLength(1); j++) 
+            { 
+                Console.Write($"{array[i, j]} "); 
+            } 
+
+            Console.WriteLine(); 
+        } 
+    } 
+ 
+    static void SortRowsDescending(int[,] array) 
+    { 
+        for (var i = 0; i < array.GetLength(0); i++) 
+        { 
+            var row = Enumerable.Range(0, array.GetLength(1))
+                                 .Select(j => array[i, j])
+                                 .OrderByDescending(x => x)
+                                 .ToArray();
+
+            for (var j = 0; j < array.GetLength(1); j++) 
+            { 
+                array[i, j] = row[j];
+            } 
+        } 
+    } 
 }
