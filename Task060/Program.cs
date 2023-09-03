c7fdd94ef782667e87f40962e5f7c4c566b67cb1
Sofya <vanieva.sofya@mail.ru>
2023-09-03 14:59:01 +0300

    Ready060

---
+++ Task060/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program 
+{ 
+    static void Main(string[] args) 
+    { 
+        int[,,] array = CreateArray(); 
+        PrintArray(array); 
+    } 
+ 
+    static int[,,] CreateArray() 
+    { 
+        int[,,] array = new int[2, 2, 2]; 
+
+        Random random = new Random();
+
+        for (var i = 0; i < 2; i++) 
+        { 
+            for (var j = 0; j < 2; j++) 
+            { 
+                for (var k = 0; k < 2; k++) 
+                { 
+                    int element = 0;
+
+                    do
+                    {
+                        element = random.Next(10, 100);
+                    } while (ArrayContains(array, element));
+
+                    array[i, j, k] = element; 
+                } 
+            } 
+        } 
+
+        return array; 
+    } 
+
+    static bool ArrayContains(int[,,] array, int element)
+    {
+        for (var i = 0; i < array.GetLength(0); i++) 
+        { 
+            for (var j = 0; j < array.GetLength(1); j++) 
+            { 
+                for (var k = 0; k < array.GetLength(2); k++) 
+                { 
+                    if (array[i, j, k] == element)
+                    {
+                        return true;
+                    }
+                } 
+            } 
+        }
+
+        return false;
+    }
+ 
+    static void PrintArray(int[,,] array) 
+    { 
+        for (var i = 0; i < 2; i++) 
+        { 
+            for (var j = 0; j < 2; j++) 
+            { 
+                for (var k = 0; k < 2; k++) 
+                { 
+                    Console.Write($"{array[i, j, k]}({i},{j},{k}) "); 
+                } 
+
+                Console.WriteLine(); 
+            } 
+        } 
+    } 
+}
